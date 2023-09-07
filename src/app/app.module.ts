db816d858908c9a898aad2f5c45b617df3901ed3
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-09-07 16:16:11 -0600

    Was added formulario component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
