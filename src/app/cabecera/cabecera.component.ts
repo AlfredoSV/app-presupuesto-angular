922f267bcd610a6dfe7562e996d308370131f2bb
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-09-11 15:52:16 -0600

    Was added props on cabecera COMPONENT

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./cabecera.component.css']
 })
 export class CabeceraComponent {
+  
+  title :string = 'Presupuesto Disponible';
+  estimateTotal : number = 2345.64;
+
+  titleIncone : string = 'Ingresos';
+  titleExpenses : string = 'Egresos';
+
+  incone  : number = 4300.00;
+  expenses : number = 1954.36;
+
+  percentageExpenses : number = 45;
 
 }
